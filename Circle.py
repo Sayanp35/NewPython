5ad436bf4d05e2443c5e62de42f581bd2d725a5e
Sayanp35 <40577318+Sayanp35@users.noreply.github.com>
2020-01-05 11:38:51 +0530

    initial commit

---
+++ Circle.py
@@ -0,0 +1,4 @@
+from math import pi
+radius=input("What is the radius = ")
+print("The radius of circle is ", float(radius) ** 2 * pi)
+print("The circumference of circle is ", 2 * float(radius) * pi)
