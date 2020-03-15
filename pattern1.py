be288f807d774bab847f952efd385a7d6876425f
Sayanp35 <40577318+Sayanp35@users.noreply.github.com>
2020-03-15 20:08:20 +0530

    First commit

---
+++ pattern1.py
@@ -0,0 +1,12 @@
+#n=input("How many stars you want : ")
+def pattern(n):
+      k = 2*n -2
+      for i in range(n,-1,-1):
+           for j in range(k,0,-1):
+                print(end=" ")
+           k = k + 1
+           for j in range(0, i+1):
+                print("*", end=" ")
+           print(" ")
+
+pattern(5)
