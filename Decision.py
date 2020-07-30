c276724b06f7e24efdbff8f53a2ad256fad93a46
Sayanp35 <40577318+Sayanp35@users.noreply.github.com>
2020-07-30 20:50:05 +0530

    Update Decision.py
    first commit

--- Decision.py
+++ Decision.py
@@ -1,24 +1,24 @@
-#a = 1 if 20 > 10 else 0
-#print ("The value of a is: " + str(a))
+a = 1 if 20 > 10 else 0
+print ("The value of a is: " + str(a))
 
 
-#i=10
-#if i>15 :
-#    print("i greater than 15")
-#print("i is less than 15")
+i=10
+if i>15 :
+    print("i greater than 15")
+print("i is less than 15")
 
 
-#i=int(input("Enter a number : "))
-#if i>15 :
-#    print("i greater than 15")
-#else:
-#    print("i is less than 15")
+i=int(input("Enter a number : "))
+if i>15 :
+    print("i greater than 15")
+else:
+    print("i is less than 15")
 
 
-#i=int(input("Enter a number : "))
-#if i>15 :
-#    print("i greater than 15")
-#elif i==15:
-#    print("i is 15")
-#else :
-#    print("i is less than 15")
+i=int(input("Enter a number : "))
+if i>15 :
+    print("i greater than 15")
+elif i==15:
+    print("i is 15")
+else :
+    print("i is less than 15")
